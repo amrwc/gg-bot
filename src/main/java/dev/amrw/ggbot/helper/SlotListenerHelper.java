5b9d240c056878304beab77f8ac3fc08afcc6f1c
amrwc <42574496+amrwc@users.noreply.github.com>
2021-01-31 14:59:34 +0000

    Slots (#3)

---
+++ src/main/java/dev/amrw/ggbot/helper/SlotListenerHelper.java
@@ -0,0 +1,53 @@
+package dev.amrw.ggbot.helper;
+
+import dev.amrw.ggbot.dto.SlotResult;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.springframework.stereotype.Component;
+
+import java.awt.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Helper class for {@link dev.amrw.ggbot.listener.SlotListener}.
+ */
+@Component
+public class SlotListenerHelper {
+
+    /**
+     * Displays each next column of the payline with a slight delay, with the final result showed after the last
+     * column has been revealed.
+     * @param event current {@link MessageCreateEvent}
+     * @param result {@link SlotResult}
+     */
+    public void displayResultSuspensefully(final MessageCreateEvent event, final SlotResult result) {
+        final var paylineFormat = "**------------------**\n**| %s | %s | %s |**\n**------------------**";
+        final var columns = new String[] {
+                result.getPayline().substring(0, 2),
+                result.getPayline().substring(2, 4),
+                result.getPayline().substring(4)
+        };
+        var future = event.getChannel().sendMessage(getEmbedBuilder(String.format(paylineFormat, "❔", "❔", "❔")));
+        final var edits = new EmbedBuilder[] {
+                getEmbedBuilder(String.format(paylineFormat, columns[0], "❔", "❔")),
+                getEmbedBuilder(String.format(paylineFormat, columns[0], columns[1], "❔")),
+                getEmbedBuilder(String.format(paylineFormat + "\n**-- YOU %s --**",
+                        columns[0], columns[1], columns[2], result.getCreditsWon() > 0L ? "WON" : "LOST"))
+                        .addField("Credits won", String.valueOf(result.getCreditsWon()))
+        };
+        for (final EmbedBuilder edit : edits) {
+            future = future.whenCompleteAsync(
+                    (message, throwable) -> message.edit(edit),
+                    CompletableFuture.delayedExecutor(1L, TimeUnit.SECONDS)
+            );
+        }
+    }
+
+    private EmbedBuilder getEmbedBuilder(final String resultContent) {
+        return new EmbedBuilder()
+                .setColor(Color.ORANGE)
+                .setTitle("Slot Machine")
+                .addField("Result", resultContent);
+    }
+}
---
+++ src/test/java/dev/amrw/ggbot/listener/SlotListenerTest.java
@@ -0,0 +1,119 @@
+package dev.amrw.ggbot.listener;
+
+import dev.amrw.ggbot.dto.SlotResult;
+import dev.amrw.ggbot.helper.SlotListenerHelper;
+import dev.amrw.ggbot.resource.BotConfig;
+import dev.amrw.ggbot.service.SlotService;
+import org.javacord.api.entity.channel.TextChannel;
+import org.javacord.api.entity.message.Message;
+import org.javacord.api.entity.message.MessageAuthor;
+import org.javacord.api.entity.message.embed.EmbedBuilder;
+import org.javacord.api.entity.user.User;
+import org.javacord.api.event.message.MessageCreateEvent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class SlotListenerTest {
+
+    @Mock
+    private SlotService service;
+    @Mock
+    private SlotListenerHelper helper;
+    @Mock
+    private BotConfig botConfig;
+    @InjectMocks
+    private SlotListener listener;
+
+    private String prefix;
+    @Mock
+    private MessageCreateEvent event;
+    @Mock
+    private Message message;
+    @Mock
+    private TextChannel channel;
+    @Mock
+    private SlotResult slotResult;
+
+    @BeforeEach
+    void beforeEach() {
+        final var trigger = randomAlphabetic(3);
+        prefix = trigger + " " + SlotListener.KEYWORD;
+        when(botConfig.getTrigger()).thenReturn(trigger);
+    }
+
+    @Test
+    @DisplayName("Should not have handled a message with wrong prefix")
+    void shouldNotHaveHandledMessageWithWrongPrefix() {
+        when(event.getMessage()).thenReturn(message);
+        when(message.getContent()).thenReturn(randomAlphanumeric(16));
+
+        listener.onMessageCreate(event);
+
+        verify(message).getContent();
+        verifyNoMoreInteractions(event, message);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", " help"})
+    @DisplayName("Should have sent help message")
+    void shouldHaveSentHelpMessage(final String messageContent) {
+        when(event.getMessage()).thenReturn(message);
+        when(message.getContent()).thenReturn(prefix + messageContent);
+        when(event.getChannel()).thenReturn(channel);
+
+        listener.onMessageCreate(event);
+
+        verify(channel).sendMessage(any(EmbedBuilder.class));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-123", "abcd", "💯💯💯"})
+    @DisplayName("Should have played a game of slots and displayed the result")
+    void shouldNotHavePlayedWithInvalidBet(final String bet) {
+        final var author = mock(MessageAuthor.class);
+        final var user = mock(User.class);
+        final var mentionTag = randomAlphanumeric(16);
+        when(event.getMessage()).thenReturn(message);
+        when(message.getAuthor()).thenReturn(author);
+        when(author.asUser()).thenReturn(Optional.of(user));
+        when(user.getMentionTag()).thenReturn(mentionTag);
+        when(message.getContent()).thenReturn(prefix + " " + bet);
+        when(event.getChannel()).thenReturn(channel);
+
+        listener.onMessageCreate(event);
+
+        verifyNoInteractions(service);
+        final var stringCaptor = ArgumentCaptor.forClass(String.class);
+        verify(channel).sendMessage(stringCaptor.capture());
+        assertThat(stringCaptor.getValue()).contains(mentionTag).contains("invalid bet");
+    }
+
+    @Test
+    @DisplayName("Should have played a game of slots and displayed the result")
+    void shouldHavePlayedAndDisplayedResult() {
+        final var bet = 100L;
+        when(event.getMessage()).thenReturn(message);
+        when(message.getContent()).thenReturn(prefix + " " + bet);
+        when(service.play(bet)).thenReturn(slotResult);
+
+        listener.onMessageCreate(event);
+
+        verify(helper).displayResultSuspensefully(event, slotResult);
+    }
+}
