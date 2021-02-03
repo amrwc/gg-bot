d06ea0a7c5605c27ec22245cb497643d49e6d4a5
amrwc <42574496+amrwc@users.noreply.github.com>
2021-02-03 16:39:08 +0000

    Fix primary key type in `UserCreditsRepository`

--- src/main/java/dev/amrw/ggbot/repository/UserCreditsRepository.java
+++ src/main/java/dev/amrw/ggbot/repository/UserCreditsRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
+import java.util.UUID;
 
 /**
  * Repository for `USER_CREDITS` table.
  */
 @Repository
 @Transactional
-public interface UserCreditsRepository extends JpaRepository<UserCredit, String> {
+public interface UserCreditsRepository extends JpaRepository<UserCredit, UUID> {
 }
