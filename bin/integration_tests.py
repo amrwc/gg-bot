99333dc62f705d0d97975fc113a51d0c061f12bf
amrwc <42574496+amrwc@users.noreply.github.com>
2021-02-13 14:02:13 +0000

    Simplify usage of `integration_tests.py`

--- bin/integration_tests.py
+++ bin/integration_tests.py
@@ -10,20 +10,10 @@ Usage:
 Options:
   -h, --help     Show this help message.
   -v, --version  Show the scripts' version.
-
-Envars:
-  SPRING_DATASOURCE_URL       Database URL.
-  SPRING_DATASOURCE_USERNAME  Database username.
-  SPRING_DATASOURCE_PASSWORD  Database password.
-
-  The above are equivalent to setting `spring.datasource.*` in `application.yml`.
-
-Example:
-  export SPRING_DATASOURCE_URL='jdbc:postgresql://test-database-container:5432/dbname'
-  export SPRING_DATASOURCE_USERNAME='springuser'
-  export SPRING_DATASOURCE_PASSWORD='SpringUserPassword'
-  ./bin/integration_tests.py
 """
+
+import os
+import secrets
 
 import docopt
 
@@ -32,29 +22,38 @@ import docker_utils
 import utils
 
 CONFIG = utils.get_config(module_path=__file__)
-REQUIRED_ENVARS = [
-    'SPRING_DATASOURCE_URL',
-    'SPRING_DATASOURCE_USERNAME',
-    'SPRING_DATASOURCE_PASSWORD',
-]
 
 
 def main() -> None:
     docopt.docopt(__doc__, version=CONFIG['DEFAULT']['script_version'])
-    utils.verify_envars(REQUIRED_ENVARS, 'Spring', __doc__)
 
-    database_container = CONFIG['DATABASE']['database_test_container']
+    db_container = CONFIG['DATABASE']['database_test_container']
     network = CONFIG['DOCKER']['network']
+    set_envars()
 
-    if not docker_utils.item_exists('network', network):
-        docker_utils.create_network(network)
-
-    database.start(container=database_container, network=network, migrations=True)
+    docker_utils.create_network(network)
+    database.start(container=db_container, network=network, migrations=True)
 
     utils.log('Running integration tests')
     utils.execute_cmd(['./gradlew', 'integrationTest', '--info'])
 
-    docker_utils.rm_container(docker_utils.DockerContainer(database_container, rm_volumes=True))
+    docker_utils.rm_container(docker_utils.DockerContainer(db_container, rm_volumes=True))
+
+
+def set_envars() -> None:
+    """Sets required envars."""
+    db_port = CONFIG['DATABASE']['port']
+    db_name = CONFIG['DOCKER']['main_image']
+    db_password = secrets.token_hex(16)
+
+    os.environ['POSTGRES_URL'] = f"jdbc:postgresql://localhost:{db_port}"
+    os.environ['POSTGRES_DB'] = db_name
+    os.environ['POSTGRES_USER'] = 'postgres'
+    os.environ['POSTGRES_PASSWORD'] = db_password
+
+    os.environ['SPRING_DATASOURCE_URL'] = f"jdbc:postgresql://localhost:{db_port}/{db_name}"
+    os.environ['SPRING_DATASOURCE_USERNAME'] = 'postgres'
+    os.environ['SPRING_DATASOURCE_PASSWORD'] = db_password
 
 
 if __name__ == '__main__':
