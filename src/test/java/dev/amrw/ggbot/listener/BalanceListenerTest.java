102bb8b77b0dce8fbd273b3ff5fbe47324df1b9e
amrwc <42574496+amrwc@users.noreply.github.com>
2021-02-16 14:19:28 +0000

    Refactor all message listeners to extend `MessageListenerBase`

--- src/test/java/dev/amrw/ggbot/listener/BalanceListenerTest.java
+++ src/test/java/dev/amrw/ggbot/listener/BalanceListenerTest.java
@@ -1,14 +1,11 @@
 package dev.amrw.ggbot.listener;
 
-import dev.amrw.ggbot.config.BotConfig;
 import dev.amrw.ggbot.model.UserCredit;
 import dev.amrw.ggbot.service.UserCreditsService;
 import dev.amrw.ggbot.util.DiscordMessageUtil;
 import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,8 +13,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.apache.commons.lang3.RandomUtils.nextLong;
 import static org.mockito.Mockito.*;
 
@@ -27,8 +22,6 @@ class BalanceListenerTest {
     @Mock
     private UserCreditsService userCreditsService;
     @Mock
-    private BotConfig botConfig;
-    @Mock
     private DiscordMessageUtil messageUtil;
     @InjectMocks
     private BalanceListener listener;
@@ -36,43 +29,23 @@ class BalanceListenerTest {
     @Mock
     private MessageCreateEvent event;
     @Mock
-    private Message message;
-    private String prefix;
-    @Mock
     private UserCredit userCredit;
     @Mock
     private EmbedBuilder embedBuilder;
     @Mock
     private TextChannel channel;
 
-    @BeforeEach
-    void beforeEach() {
-        final var trigger = randomAlphabetic(3);
-        prefix = trigger + " " + BalanceListener.KEYWORD;
-        when(event.getMessage()).thenReturn(message);
-        when(botConfig.getTrigger()).thenReturn(trigger);
-    }
-
-    @Test
-    @DisplayName("Should not have handled a message with wrong prefix")
-    void shouldNotHaveHandledMessageWithWrongPrefix() {
-        when(message.getContent()).thenReturn(randomAlphanumeric(16));
-        listener.onMessageCreate(event);
-        verifyNoMoreInteractions(event, message);
-    }
-
     @Test
     @DisplayName("Should have displayed the user's credits balance")
     void shouldHaveDisplayedCreditsBalance() {
         final Long credits = nextLong();
-        when(message.getContent()).thenReturn(prefix);
         when(event.getChannel()).thenReturn(channel);
         when(userCreditsService.getOrCreateUserCredit(event)).thenReturn(userCredit);
         when(messageUtil.buildEmbedInfo(event, "Credits Balance")).thenReturn(embedBuilder);
         when(userCredit.getCredits()).thenReturn(credits);
         when(embedBuilder.setDescription(credits.toString())).thenReturn(embedBuilder);
 
-        listener.onMessageCreate(event);
+        listener.process(event);
 
         verify(channel).sendMessage(embedBuilder);
         verifyNoMoreInteractions(userCreditsService, messageUtil, channel);
--- src/test/java/dev/amrw/ggbot/listener/DailyListenerTest.java
+++ src/test/java/dev/amrw/ggbot/listener/DailyListenerTest.java
@@ -1,13 +1,11 @@
 package dev.amrw.ggbot.listener;
 
-import dev.amrw.ggbot.config.BotConfig;
 import dev.amrw.ggbot.dto.DailyCreditsResult;
 import dev.amrw.ggbot.dto.Error;
 import dev.amrw.ggbot.model.UserCredit;
 import dev.amrw.ggbot.service.DailyService;
 import dev.amrw.ggbot.util.DiscordMessageUtil;
 import org.javacord.api.entity.channel.TextChannel;
-import org.javacord.api.entity.message.Message;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 import org.javacord.api.event.message.MessageCreateEvent;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,18 +16,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static org.apache.commons.lang3.RandomUtils.nextLong;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DailyListenerTest {
 
     @Mock
     private DailyService dailyService;
-    @Mock
-    private BotConfig botConfig;
     @Mock
     private DiscordMessageUtil messageUtil;
     @InjectMocks
@@ -38,33 +34,17 @@ class DailyListenerTest {
     @Mock
     private MessageCreateEvent event;
     @Mock
-    private Message message;
-    @Mock
     private UserCredit userCredit;
     @Mock
     private EmbedBuilder embedBuilder;
     @Mock
     private TextChannel channel;
 
-    private String prefix;
     private Long credits;
 
     @BeforeEach
     void beforeEach() {
-        final var trigger = randomAlphabetic(3);
-        prefix = trigger + " " + DailyListener.KEYWORD;
         credits = nextLong();
-        when(event.getMessage()).thenReturn(message);
-        when(botConfig.getTrigger()).thenReturn(trigger);
-    }
-
-    @Test
-    @DisplayName("Should not have handled a message with wrong prefix")
-    void shouldNotHaveHandledMessageWithWrongPrefix() {
-        when(message.getContent()).thenReturn(randomAlphanumeric(16));
-        when(botConfig.getTrigger()).thenReturn("");
-        listener.onMessageCreate(event);
-        verifyNoMoreInteractions(event, message);
     }
 
     @Test
@@ -74,12 +54,16 @@ class DailyListenerTest {
         final var dailyCreditsResult = new DailyCreditsResult(0L, userCredit, error);
         final var nextDailyIn = randomAlphanumeric(16);
 
+        when(dailyService.claimDailyCredits(event)).thenReturn(dailyCreditsResult);
+
         when(messageUtil.buildEmbedError(event, error)).thenReturn(embedBuilder);
         when(userCredit.getTimeLeftUntilNextDaily()).thenReturn(nextDailyIn);
         when(embedBuilder.addField("Next daily in", nextDailyIn)).thenReturn(embedBuilder);
-        addCommonStubbings(dailyCreditsResult);
 
-        listener.onMessageCreate(event);
+        when(userCredit.getCredits()).thenReturn(credits);
+        when(event.getChannel()).thenReturn(channel);
+
+        listener.process(event);
 
         verify(embedBuilder).addField("Current balance", credits.toString());
         verify(channel).sendMessage(embedBuilder);
@@ -91,21 +75,17 @@ class DailyListenerTest {
         final Long claimedCredits = nextLong();
         final var dailyCreditsResult = new DailyCreditsResult(claimedCredits, userCredit, null);
 
+        when(dailyService.claimDailyCredits(event)).thenReturn(dailyCreditsResult);
+
         when(messageUtil.buildEmbedInfo(event, "Daily Credits")).thenReturn(embedBuilder);
         when(embedBuilder.addField("New credits", claimedCredits.toString())).thenReturn(embedBuilder);
-        addCommonStubbings(dailyCreditsResult);
-
-        listener.onMessageCreate(event);
-
-        verify(embedBuilder).addField("Current balance", credits.toString());
-        verify(channel).sendMessage(embedBuilder);
-    }
-
-    private void addCommonStubbings(final DailyCreditsResult dailyCreditsResult) {
-        when(message.getContent()).thenReturn(prefix);
-        when(dailyService.claimDailyCredits(event)).thenReturn(dailyCreditsResult);
 
         when(userCredit.getCredits()).thenReturn(credits);
         when(event.getChannel()).thenReturn(channel);
+
+        listener.process(event);
+
+        verify(embedBuilder).addField("Current balance", credits.toString());
+        verify(channel).sendMessage(embedBuilder);
     }
 }
